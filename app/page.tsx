b5406c088a1eb6e2402ff8309a0a93e390a987d4
victoryejike <victoryejike@gmail.com>
2023-10-10 04:48:29 +0100

    added empty state component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,43 @@
 import Image from "next/image";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
 export default function Home() {
-	return <div className="text-2xl">Hello AirBnB users</div>;
+  const isEmpty = true;
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          Future listings
+          {/* {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))} */}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
